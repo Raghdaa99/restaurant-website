6d04f9cdb561a53453150ded08d30cff6752b445
TareqZiadd <tareqziad1001@gmail.com>
2025-02-22 13:55:58 -0800

    Complete Sign Up page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+  import { createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: () => import("@/views/Home.vue"),
-    },
-    { 
-      path: "/contact",
-      name: "Contact", 
-      component: () => import("@/views/ContactUs.vue"),
-    },
-    { 
-      path: "/reservation",
-      name: "Reservation", 
-      component: () => import("@/views/Reservation.vue"),
-    },
-  ],
-})
+  const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: "/",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
+      },
+      { 
+        path: "/contact",
+        name: "Contact", 
+        component: () => import("@/views/ContactUs.vue"),
+      },
+      { 
+        path: "/reservation",
+        name: "Reservation", 
+        component: () => import("@/views/Reservation.vue"),
+      },  { 
+        path: "/signup",
+        name: "SignUp", 
+        component: () => import("@/components/auth/SignUp.vue"),
+      },
+    ],
+  })
 
-export default router
+  export default router
